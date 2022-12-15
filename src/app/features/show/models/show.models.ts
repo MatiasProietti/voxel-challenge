8fd69b2a6ce7bdd55b102a39ac9b3245be8c55b1
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2022-12-15 02:50:25 -0300

    feat(show): added show models
    Added show models for backend and front
    Added a ShowAdapter class
    Added types and interfaces used for these models

---
+++ src/app/features/show/models/show.models.ts
@@ -0,0 +1,59 @@
+export type ShowImage = {
+  medium: string;
+  original: string;
+};
+
+export type ShowRating = {
+  average: number;
+};
+
+/*
+  Depending on coding styles the types above could've been moved to a different
+    file inside a "types" folder
+*/
+
+export interface ElementOnListOfShowBackend {
+  score: number;
+  show: ShowBackend;
+}
+
+export interface ShowBackend {
+  id: number;
+  name: string;
+  image: ShowImage;
+  rating: ShowRating;
+  summary: string;
+  genres: string[];
+  // ... and more but for this excercise we only care about those 6.
+}
+
+export class Show {
+  public readonly id: number;
+  public name: string;
+  public image: ShowImage;
+  public rating: ShowRating;
+  public summary: string;
+  public genres: string[];
+
+  constructor(id: number, name: string, image: ShowImage, rating: ShowRating, summary: string, genres: string[]) {
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.rating = rating;
+    this.summary = summary;
+    this.genres = genres;
+  }
+}
+
+export class ShowAdapter {
+  static fromBackend(showFromBackend: ShowBackend): Show {
+    return new Show(
+      showFromBackend.id,
+      showFromBackend.name,
+      showFromBackend.image,
+      showFromBackend.rating,
+      showFromBackend.summary,
+      showFromBackend.genres
+    );
+  }
+}
