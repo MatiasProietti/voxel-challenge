a16d3f6771f1299ecad46c44e0ef800db6d0f23a
MatiasProietti <95661104+MatiasProietti@users.noreply.github.com>
2022-12-15 02:52:03 -0300

    chore(app): added redirection to route show

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'show', loadChildren: () => import('./features/show/show.module').then((m) => m.ShowModule) }];
+const routes: Routes = [
+  { path: 'show', loadChildren: () => import('./features/show/show.module').then((m) => m.ShowModule) },
+  { path: '**', redirectTo: 'show' },
+];
+// Routes names could be moved to a constants file.
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
